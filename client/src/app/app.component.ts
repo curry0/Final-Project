520aee799e4fc02b8320227ed8fefa516a6d57bf
Robert Kocev <robert.kocev@actual-it.si>
2023-03-30 16:01:30 +0200

    API and client basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GymApp';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.log(err),
+      complete: () => console.log('completed')
+    })
+  }
+
 }
