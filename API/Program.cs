fecc75d0a81736a40bae9f02fd016cc3e6f1a4ac
RK <robertkocev9@gmail.com>
2023-04-03 19:24:25 +0200

    Implementing AutoMapper and added static files

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddCors();
 
@@ -18,6 +19,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
