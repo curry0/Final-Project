fabc328c193e00f3abbcb3d0f1385f8e68425efa
Robert Kocev <robertkocev9@gmail.com>
2023-04-21 08:31:08 +0200

    Editing roles

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -13,6 +13,11 @@ import { initialState } from 'ngx-bootstrap/timepicker/reducer/timepicker.reduce
 export class UserManagementComponent implements OnInit {
     users: User[] = [];
     bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
+    availableRoles = [
+        'Admin',
+        'Moderator',
+        'Member'
+    ]
 
     constructor(private adminService: AdminService, private modalService: BsModalService) {}
 
@@ -29,18 +34,29 @@ export class UserManagementComponent implements OnInit {
         })
     }
 
-    openRolesModal() {
-        const initialState: ModalOptions = {
+    openRolesModal(user: User) {
+        const config = {
+            class: 'modal-dialog-centered',
             initialState: {
-                list: [
-                    'Do thing',
-                    'Do thing',
-                    'Do thing'
-                ],
-                title: 'Test modal'
+                username: user.username,
+                availableRoles: this.availableRoles,
+                selectedRoles: [...user.roles]
             }
         }
-        this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
-        this.bsModalRef.content!.closeBtnName = 'Close';
+        this.bsModalRef = this.modalService.show(RolesModalComponent, config);
+        this.bsModalRef.onHide?.subscribe({
+            next: () => {
+                const selectedRoles = this.bsModalRef.content?.selectedRoles;
+                if (!this.arrayEqual(selectedRoles!, user.roles)) {
+                    this.adminService.updateUserRoles(user.username, selectedRoles!).subscribe({
+                        next: roles => user.roles = roles
+                    })
+                }
+            }
+        })
+    }
+
+    private arrayEqual(arr1: any[], arr2: any[]) {
+        return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort());
     }
 }
--- client/src/app/shared/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/shared/modals/roles-modal/roles-modal.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-roles-modal',
-  templateUrl: './roles-modal.component.html',
-  styleUrls: ['./roles-modal.component.scss']
+    selector: 'app-roles-modal',
+    templateUrl: './roles-modal.component.html',
+    styleUrls: ['./roles-modal.component.scss']
 })
 export class RolesModalComponent {
+    username = '';
+    availableRoles: any[] = [];
+    selectedRoles: any[] = [];
 
     constructor(public bsModalRef: BsModalRef) { }
-    title = ''
-    list: any;
-    closeBtnName = ''
+
+    updateChecked(checkValue: string) {
+        const index = this.selectedRoles.indexOf(checkValue);
+        index !== -1 ? this.selectedRoles.splice(index, 1) : this.selectedRoles.push(checkValue);
+    }
 
 }
